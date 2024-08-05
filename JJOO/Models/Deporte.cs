3a167182e9257eb2f5d06fcbf0cc3cf26fd6148f
TOMAS DAVID KLAWIR <48385206@ort>
2024-08-05 09:59:05 -0300

    Clases Deporte y Pais hechas

---
+++ JJOO/Models/Deporte.cs
@@ -0,0 +1,5 @@
+class Deporte{
+    public int IdDeporte { get; set; }
+    public string Nombre { get; set; }
+    public string Foto { get; set; }
+}
---
+++ JJOO/Models/Pais.cs
@@ -0,0 +1,6 @@
+class Pais{
+    public int IdPais { get; set; }
+    public string Nombre { get; set; }
+    public string Bandera { get; set; }
+    public DateTime FechaFundacion { get; set; }
+}
