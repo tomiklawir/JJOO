713f2bce890d5b19d8abee3f9dec28bf7bb9295f
TOMAS DAVID KLAWIR <48385206@ort>
2024-08-05 10:02:16 -0300

    Clase Deportista hecha

---
+++ JJOO/Models/Deportista.cs
@@ -0,0 +1,9 @@
+class Deportista{
+    public int IdDeportista { get; set; }
+    public string Apellido { get; set; }
+    public string Nombre { get; set; }
+    public DateTime FechaNacimiento { get; set; }
+    public string Foto { get; set; }
+    public int IdPais { get; set; }
+    public int IdDeporte { get; set; }
+}
